4853c496d3e47992d06bb009fc8fe6ef4f7a119d
matheus mendes <matheus_mendess@live.com>
2023-02-27 23:28:26 -0300

    Projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 function consultaCep() {
+  $(".barra-progresso").show();
   var cep = document.getElementById("cep").value;
-  var url = "https://viacep.com.br/ws/" + { cep } + "/json/";
+
+  if(cep.length === 0){
+    $(".alerta-cep-vazio").show();
+  }
+
+  var url = "https://viacep.com.br/ws/" + cep  + "/json/";
 
   console.log(url);
   $.ajax({
@@ -11,6 +17,20 @@ function consultaCep() {
     success: function (response) {
       console.log(response);
       $("#logradouro").html(response.logradouro);
+      $("#bairro").html(response.bairro);
+      $("#localidade").html(response.localidade);
+      $("#uf").html(response.uf);
+      $("#titulo_cep").html("CEP: " + response.cep);
+      $(".cep").show();
+      $(".barra-progresso").hide();
+      $(".alerta-cep-vazio").hide();
     },
   });
 }
+
+
+$(function(){
+  $(".cep").hide();
+  $(".barra-progresso").hide();
+  $(".alerta-cep-vazio").hide();
+})
